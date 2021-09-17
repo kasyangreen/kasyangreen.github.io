64b0d297a109583c99dcd1b532398db0846469c6
kgreen97 <kasyangreen@hotmail.com>
2021-09-17 11:18:02 -0400

    Completed Task 4. Operators

--- javascript/external.js
+++ javascript/external.js
@@ -17,16 +17,25 @@
 //document.write(out);
 
 
-//A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+// //A prompt box is used to prompt users to input a value before entering a page.
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// document.write(user_name);
+//
+// // there are many ways to use the prompt feature
+// sign = window.prompt(); // open the blank prompt window
+// sign = prompt(); // open the blank prompt window
+// sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
+// sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
+//
+// // A prompt box is used to prompt users to input a value before entering a page.
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// document.write(user_name);
 
-// there are many ways to use the prompt feature
-sign = window.prompt(); // open the blank prompt window
-sign = prompt(); // open the blank prompt window
-sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
-sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
 
-// A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+//Task 4:Operators
+
+x = 22;
+const y =33;
+document.writeln(x + y);
+document.writeln("<br>";
+document.writeln(x += 33);
