aa72ae30d3b14358a5b5ea3e43a7635aa29e2364
kgreen97 <kasyangreen@hotmail.com>
2021-09-17 11:15:20 -0400

    Completed Task 3. Variables

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,32 @@
+
+//Task 1: How to embed your JS code in HTML pages
 //document.write("Where is my JS code?");
+
+
+//Task 2: Output
 //window.alert(5 + 6);
 
-document.write("<h1>A heading</h1>");
-document.write("<p>A sentence.</p>");
+//document.write("<h1>A heading</h1>");
+//document.write("<p>A sentence.</p>");
+
+
+//Task 3: Variables
+//const x = "web";
+//const y = "mapping";
+//const out = x + y;
+//document.write(out);
+
+
+//A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
+
+// there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+sign = prompt(); // open the blank prompt window
+sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
+sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
+
+// A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
