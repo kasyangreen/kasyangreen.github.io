8dfd2500dbafe729aa5cf09b185cd1f9a9e0d18c
kgreen97 <kasyangreen@hotmail.com>
2021-09-24 10:42:05 -0400

    Defined a function for the welcome message.

--- javascript/external.js
+++ javascript/external.js
@@ -1,75 +1,13 @@
+function welcome()
+{
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
 
-//Task 1: How to embed your JS code in HTML pages
-//document.write("Where is my JS code?");
+  //a prompt box to allow users to enter their name
+  user_name = window.prompt(a, b);
 
+  message = "<h1> Hello, Welcome to my webpage, "+ user_name + "!</h1>"
+  return message
+}
 
-
-//Task 2: Output
-//window.alert(5 + 6);
-
-//document.write("<h1>A heading</h1>");
-//document.write("<p>A sentence.</p>");
-
-
-
-//Task 3: Variables
-//const x = "web";
-//const y = "mapping";
-//const out = x + y;
-//document.write(out);
-
-// //A prompt box is used to prompt users to input a value before entering a page.
-// user_name = window.prompt("Please enter your name", "Type your name here");
-// document.write(user_name);
-//
-// // there are many ways to use the prompt feature
-// sign = window.prompt(); // open the blank prompt window
-// sign = prompt(); // open the blank prompt window
-// sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
-// sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
-//
-// // A prompt box is used to prompt users to input a value before entering a page.
-// user_name = window.prompt("Please enter your name", "Type your name here");
-// document.write(user_name);
-
-
-
-//Task 4:Operators
-//x = 22;
-//const y =33;
-//document.writeln(x + y);
-//document.writeln("<br>");
-//document.writeln(x += 33);
-
-
-
-//Task 5: Conditional statement
-// document.writeln("<button onclick='condition()''>Conditional Test</button>")
-//
-// function condition()
-// {
-//   x = confirm("Are you sure you want to proceed?");
-//   if(x)
-//   {
-//     document.writeln("You chose Okay!");
-//   }
-//   else
-//   {
-//     document.writeln("You chose Cancel!");
-//   }
-// }
-
-//Task 6: Array
-// arr = ["Joe","April","Mark","Sara"];
-// for (var i=0;
-//   i < arr.length;
-//   i++)
-//   {
-//     document.writeln(arr[i],"<br>");
-//   }
-
-
-// Homework Assignment
-
-name = window.prompt("What is your name", "Enter your name here");
-document.write(name);
+document.write(welcome());
