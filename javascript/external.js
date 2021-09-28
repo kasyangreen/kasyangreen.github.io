fc2954be8159f92382d066f0830c26b7e8df9ae5
kgreen97 <kasyangreen@hotmail.com>
2021-09-28 18:16:29 -0400

    Used conditional statements within for loops to make table look more aesthetic

--- javascript/external.js
+++ javascript/external.js
@@ -32,7 +32,14 @@ function webmap_table()
     document.write("<tr>");
     for (var column=0; column < webmaps[0].length; column++)
     {
-      document.write("<td>" + webmaps[row][column] + "</td>");
+
+      if (column > 1)
+      {
+        document.write("<td width='200px'>" + webmaps[row][column] + "</td>");
+      }
+      else {
+        document.write("<td width='20px'>" + webmaps[row][column] + "</td>");
+      }
     }
     document.write("</tr>");
   }
